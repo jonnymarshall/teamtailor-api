87defd4d4e53b122daf3930ee02b5461720bf073
Jonny Marshall <jonnymarshall5@gmail.com>
2020-06-12 10:48:33 +0100

    index page migrated to react

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,15 +1,65 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom'
-import Jobs from './Jobs/Jobs'
-import Job from './Job/Job'
+import Job from '../components/Job'
 import Favourites from './Favourites/Favourites'
 
 
 const App = () => {
   return (
-    <div>
-      <h1>hello react</h1>
-    </div>
+    <React.Fragment>
+      <section className="hero is-medium hero-image">
+        <div className="hero-body">
+          <div className="container has-text-centered u-transition-all-1s" data-controller="homepage-search-position" data-target="homepage-search-position.centralContainer">
+            <img className="hero-logo" src="https://ember.cdn.teamtailor.com/ember-production/assets/images/teamtailor-logo-f442e875406f0a21cddfe9f211283b92.svg"></img>
+            <div className="columns">
+              <div className="column is-paddingless"></div>
+              <div className="column is-two-fifths" data-controller="algolia-places">
+                <form className="form-group col-xs-6 c-field-has-autocomplete-results" action="/venues" acceptCharset="UTF-8" method="get">
+                  <input name="utf8" type="hidden" value="✓"></input>
+                  <div className="field has-addons">
+                    <p className="control is-expanded">
+                      <input
+                        className="input is-primary has-text-centered"
+                        placeholder="Search for your dream role"
+                        data-target="jobs-index.searchInput"
+                        data-request-path="/location_search"
+                        data-action="keyup->jobs-index#searchHandler"
+                        type="text"
+                        name="location"
+                        id="location">
+                      </input>
+                    </p>
+                  </div>
+                  <div
+                    className="bu-prev-element-margin-b-counter c-autocomplete-results-container"
+                    data-target="algolia-places.resultsContainer homepage-search-position.resultsContainer ">
+                  </div>
+                </form>
+              </div>
+              <div className="column is-paddingless"></div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-three-fifths is-offset-one-fifth">
+              <div className="tags" data-controller="tags departments">
+                <div className="field is-grouped is-grouped-multiline" data-target="departments.tagsContainer">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container"  data-controller="add-favourite" data-target="jobs-index.container add-favourite.container" data-url="favourites_path">
+          <div className="columns">
+            <Job />
+          </div>
+        </div>
+      </section>
+    </React.Fragment>
   );
 }
 
---
+++ app/javascript/components/Job.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+const Job = () => {
+  return (
+    <div className="column is-three-fifths is-offset-one-fifth">
+      <div className="c-job-main-section is-flex">
+        <figure className="image is-128x128 u-padding-10">
+          <img src="https://images.unsplash.com/photo-1487528278747-ba99ed528ebc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3450&q=80"></img>
+        </figure>
+        <div id="c-job--right-section" className="is-flex u-padding-10">
+          <div id="content" className="is-flex">
+            <div id="job-title-and-description">
+              <h1 className="title is-4">Title</h1>
+              <h2 className="subtitle is-5">Pitch</h2>
+            </div>
+            <div id="icons">
+              <span className="icon has-text-grey-dark">
+                <a href = "mailto:abc@example.com">
+                  <i className="fas fa-lg fa-envelope"></i>
+                </a>
+              </span>
+              <span
+                className="icon has-text-grey-dark u-pointer"
+                data-action="click->add-favourite#toggleFavourite"
+                data-target="add-favourite.button"
+                data-job-id="1"
+              >
+                <i className="fas fa-lg fa-heart"></i>
+              </span>
+            </div>
+          </div>
+          <div id="footer" className="is-flex">
+            <div className="buttons">
+              <button className="button is-small is-primary is-rounded">View</button>
+              <button className="button is-small is-secondary is-rounded">Visit</button>
+              <button className="button is-small is-link is-rounded">Apply</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+ 
+export default Job;
